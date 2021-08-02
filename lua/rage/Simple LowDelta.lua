39cd2ff14b90cf6e127d50e73094771c06074f94
Fla1337 <ccharles_0713@yahoo.com>
2021-08-02 23:08:17 +0800

    Update Simple LowDelta.lua

--- lua/rage/Simple LowDelta.lua
+++ lua/rage/Simple LowDelta.lua
@@ -1,12 +1,14 @@
 local lowdelta = ui.add_key_bind("Low Delta On Key","low_delta", 0, 1)
 local desync = ui.get_slider_int("antihit_antiaim_desync_length")
+local desync_lua = ui.add_slider_int("Desync length", "desync_lua", 0, 60, 40)
 
 local function cydelta()
 
     if lowdelta:is_active() then
         desync:set_value(10) --- Desync value when lowdelta bind actived
     else
-        desync:set_value(40) --- Desync value when lowdelta bind not actived
+        local desync_backup = desync_lua:get_value()
+        desync:set_value(desync_backup) --- Desync value when lowdelta bind not actived
     end
 end
 
