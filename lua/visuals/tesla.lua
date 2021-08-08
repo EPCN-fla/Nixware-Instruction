e9bc48be3f48db0365fd1f2cb42064224d775fb8
Fla1337 <ccharles_0713@yahoo.com>
2021-08-08 12:41:46 +0800

    Update tesla.lua

--- lua/visuals/tesla.lua
+++ lua/visuals/tesla.lua
@@ -1,3 +1,10 @@
+enable      = ui.add_check_box("Tesla on hit", "enable_tesla", false)
+color       = ui.add_color_edit("Color", "color_tesla", false, color_t.new(255, 255, 255, 255))
+width       = ui.add_slider_int("Tesla width", "width_tesla", 0, 30, 10)
+radius      = ui.add_slider_int("Tesla radius", "radius_tesla", 0, 1000, 500)
+slider      = ui.add_slider_int("Beams", "beams_tesla", 0, 100, 12)
+sprite      = ui.add_check_box("Other sprite", "sprite_tesla", true)
+
 ffi.cdef[[ 
 		typedef struct { 
 			float x,y,z; 
@@ -17,39 +24,34 @@ ffi.cdef[[
 		
 		typedef void(__thiscall* FX_TeslaFn)(struct tesla_info_t&); 
 ]] 
+
 local match = client.find_pattern("client.dll", "55 8B EC 81 EC ? ? ? ? 56 57 8B F9 8B 47 18")
 local fs_tesla = ffi.cast("FX_TeslaFn", match)
 
 local uidtoentindex = engine.get_player_for_user_id
 local get_local_player = engine.get_local_player()
-ui.add_checkbox("Tesla on hit", "enable_tesla", false)
-ui.add_color_edit("Color", "color_tesla", false, color_t.new(255, 255, 255, 255))
-ui.add_slider_int("Tesla width", "width_tesla", 0, 30, 10)
-ui.add_slider_int("Tesla radius", "radius_tesla", 0, 1000, 500)
-ui.add_slider_int("Beams", "beams_tesla", 0, 100, 12)
-ui.add_checkbox("Other sprite", "sprite_tesla", true)
-local function on_fire_game_event(e)
-if e:get_name() == "player_hurt" then
-    if ui.get_bool("enable_tesla") then
-        local me = get_local_player
-        local attacker = uidtoentindex(e:get_int("attacker", 0))
-        if attacker == me then
-            local hurt = uidtoentindex(e:get_int("userid", 0))
-            local r,g,b = ui.get_color("color_tesla").r, ui.get_color("color_tesla").g, ui.get_color("color_tesla").b
-            local x,y,z = 0,0,0
-            local tesla_info = ffi.new("struct tesla_info_t")
-            tesla_info.m_flbeamwidth = ui.get_int("width_tesla")
-			tesla_info.m_flradius = ui.get_int("radius_tesla")
-			tesla_info.m_entindex = attacker
-			tesla_info.m_color = {r/255, g/255, b/255}
-            tesla_info.m_pos = { entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).x, entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).y, entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).z }
-            tesla_info.m_ang = {x,y,z} 
-			tesla_info.m_fltimevis = 0.75 
-			tesla_info.m_nbeams = ui.get_int("beams_tesla")
-            tesla_info.m_spritename = ui.get_bool("sprite_tesla") and "sprites/physbeam.vmt" or "sprites/purplelaser1.vmt"
-            fs_tesla(tesla_info)
-        end
-    end
-end
-end
-client.register_callback('fire_game_event', on_fire_game_event)
+
+client.register_callback("fire_game_event", function(e)
+	if e:get_name() == "player_hurt" then
+		if enable:get_value() then
+			local me = get_local_player
+            local attacker = uidtoentindex( e:get_int("attacker", 0) )
+			if attacker == me then
+				local hurt = uidtoentindex( e:get_int( "userid", 0 ) )
+				local r, g, b = color:get_value().r, color:get_value().g, color:get_value().b
+				local x, y, z = 0,0,0
+				local tesla_info = ffi.new( "struct tesla_info_t" )
+				tesla_info.m_flbeamwidth = width:get_value()
+				tesla_info.m_flradius = radius:get_value()
+				tesla_info.m_entindex = attacker
+				tesla_info.m_color = { r/255, g/255, b/255 }
+				tesla_info.m_pos = { entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).x, entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).y, entitylist.get_entity_by_index(uidtoentindex(e:get_int("userid", 0))):get_player_hitbox_pos(6).z }
+				tesla_info.m_ang = {x, y, z} 
+				tesla_info.m_fltimevis = 0.75 
+				tesla_info.m_nbeams = slider:get_value()
+				tesla_info.m_spritename = sprite:get_value() and "sprites/physbeam.vmt" or "sprites/purplelaser1.vmt"
+				fs_tesla(tesla_info)
+			end
+		end
+	end
+end)
